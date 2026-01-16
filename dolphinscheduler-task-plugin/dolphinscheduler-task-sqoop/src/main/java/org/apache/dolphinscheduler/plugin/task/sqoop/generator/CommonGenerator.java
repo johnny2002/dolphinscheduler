81641770a7581e52f294248349f624a5b41a8909
johnnyzhou <johnny.zhou@163.com>
2026-01-16 13:49:04 +0800

    sqoop多一个引号

--- dolphinscheduler-task-plugin/dolphinscheduler-task-sqoop/src/main/java/org/apache/dolphinscheduler/plugin/task/sqoop/generator/CommonGenerator.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-sqoop/src/main/java/org/apache/dolphinscheduler/plugin/task/sqoop/generator/CommonGenerator.java
@@ -51,7 +51,6 @@ public class CommonGenerator {
                     .append(SqoopConstants.LIB_JARS)
                     .append(SPACE)
                     .append(DOUBLE_QUOTES)
-                    .append(DOUBLE_QUOTES)
                     .append(SqoopConstants.JAR_FILES)
                     .append(DOUBLE_QUOTES);
 
