cec828cc0f2476ee423630ebc9c30eac285d6c37
johnnyzhou <johnny.zhou@163.com>
2026-01-16 19:03:31 +0800

    datax sql 参数替换

--- dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/pre/DataXDelegateDataSourcePreTaskCreationHandler.java
+++ dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/pre/DataXDelegateDataSourcePreTaskCreationHandler.java
@@ -33,6 +33,11 @@ public class DataXDelegateDataSourcePreTaskCreationHandler extends AbstractDeleg
             taskParams.setDataTarget(dataSource.getId());
             taskParams.setDtType(dataSource.getType().name());
         }
+        if (taskParams.getSql() != null && taskParams.getSql().matches("^\\$\\{.+\\}")) {
+            String sql =  taskParams.getSql();
+            sql = resolveSpel(sql, env);
+            taskParams.setSql(sql);
+        }
         return JSONUtils.toJsonString(taskParams);
     }
 
